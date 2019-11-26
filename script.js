02fb42878390032fc639090c8c974a120e0a21d7
nathalie <pillennathalie@gmail.com>
2019-11-26 13:52:29 +0100

    update enter function

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-let cocktailSearch = document.getElementById("search");
+const input = document.getElementById("cocktailInput");
+const cocktailSearch = document.getElementById("search");
+
+// eventlistener for enter
+
+input.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        cocktailSearch.click();
+    }
+})
+
+
+// eventlistener for button
 
 cocktailSearch.addEventListener(("click"), function() {
 
@@ -10,54 +23,69 @@ cocktailSearch.addEventListener(("click"), function() {
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            //console.log(data);
             return data.drinks;
         })
         .then(list => {
             console.log(list);
-            for (i = 0; i < list.length; i++) {
+
+            // select only the first object of the array
+            for (i = 0; i <= 0; i++) {
 
                 let cocktailName = list[i].strDrink;
                 //console.log(cocktailName);
-                let cocktailImage = list[i].strDrinkThumb;
-                //console.log(cocktailImage);
-                let cocktailCategory = list[i].strAlcoholic + "<br>" + list[i].strCategory;
-                //console.log(cocktailCategory);
-
-                //str.Measure1 + str.Ingredients1 next lin str.Measure2 + str.Ingredients2 ...
-                let cocktailIngredients = list[i].strMeasure + " " + list[i].strIngredients;
-                console.log(cocktailIngredients);
-
-
-                let cocktailInstructions = list[i].strInstructions;
-                //console.log(cocktailInstructions);
-                let cocktailGlass = list[i].strGlass;
-                //console.log(cocktailGlass);
-
-
                 let nameElement = document.querySelector(".cocktailName p");
                 //console.log(nameElement);
                 nameElement.innerHTML = cocktailName;
 
+
+                let cocktailImage = list[i].strDrinkThumb;
+                //console.log(cocktailImage);
                 let imageElement = document.querySelector(".cocktailImage");
                 //console.log(imageElement);
                 imageElement.src = cocktailImage;
 
+
+                let cocktailCategory = list[i].strAlcoholic + "<br>" + list[i].strCategory;
+                //console.log(cocktailCategory);
                 let categoryElement = document.querySelector(".cocktailCategory p");
                 //console.log(categoryElement);
                 categoryElement.innerHTML = cocktailCategory;
 
+
+                let cocktailInstructions = list[i].strInstructions;
+                //console.log(cocktailInstructions);
                 let instructionsElement = document.querySelector(".cocktailInstructions p");
+                //console.log(instructionsElement);
                 instructionsElement.innerHTML = cocktailInstructions;
 
+                let cocktailGlass = list[i].strGlass;
+                //console.log(cocktailGlass);
                 let glassElement = document.querySelector(".cocktailGlass p");
+                //console.log(glassElement);
                 glassElement.innerHTML = cocktailGlass;
+
+
+                /*const getIngredients = list[i] => {
+                    let ingredient = [];
+                    let i = 1;
+
+                    do {
+                        const cocktailIngredient = list[i][`strIngredient${i}`];
+                        const cocktailMeasure = list[i][`strMeasure${i}`];
+                        ingredients.push(`${cocktailMeasure} ${cocktailIngredient}`);
+                        console.log(ingredients);
+                        i++;
+                    } while (list[i][`strIngredient${i}`] !== null);
+                }*/
+
             }
 
         })
-        .catch(err => {
-            console.log(err)
-        });
+
+    .catch(err => {
+        console.log(err)
+    });
 
 
 
